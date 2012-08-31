4988106ad31ffd1b54e591e324464ee7b8e79b6d
tomykaira <tomykaira@gmail.com>
2012-08-31 18:51:20 +0900

    Create first example

---
+++ simulator/src/main/groovy/Main.groovy
@@ -0,0 +1,13 @@
+package groovy
+
+/**
+ * Main entry point
+ * User: tomykaira
+ * Date: 8/31/12
+ * Time: 6:41 PM
+ */
+class Main {
+    public static void main(String[] args) {
+        println "hello world"
+    }
+}
