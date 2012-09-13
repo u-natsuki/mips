d5636ade0200097e6fdd5998fc59478bc01b939d
tomykaira <tomykaira@gmail.com>
2012-09-13 15:34:28 +0900

    Remove decoder in simulator

--- testbench_endtoend.v
+++ testbench_endtoend.v
@@ -5,9 +5,6 @@ module testbench_endtoend();
    reg rs_rx;
    wire rs_tx;
 
-   wire check_changed;
-   wire [7:0] check_data;
-
    // for sram
    wire [31:0] ZD;
    wire [3:0]  ZDP;
@@ -28,9 +25,6 @@ module testbench_endtoend();
             .ADVA(ADVA), .XFT(XFT), .XLBO(XLBO), .ZZA(ZZA),
 
             .CLK(clk), .XRST(xreset), .RS_RX(rs_rx), .RS_TX(rs_tx));
-
-   // in post-map simulation, other modules are not available.
-   i232c #(.wtime(16'h008F)) decoder(.clk(clk), .rx(rs_tx), .data(check_data), .changed(check_changed));
 
    integer i;
    task send;
